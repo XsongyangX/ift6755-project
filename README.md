c5b29802c152ed91c25e38b35776cc020d4eab47
Song Yang <songyang1218@gmail.com>
2021-04-28 15:36:38 -0400

    Grouping data

---
+++ README.md
@@ -0,0 +1,5 @@
+# IFT6755 Project: Automatic translation of prose into UML class diagrams
+This project repo demonstrates how to translate English into UML class diagrams by using neural models.
+
+## Data
+The data set comes the handwritten [data set](https://github.com/XsongyangX/UML-hand-class-diagrams).
---
+++ group.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Be sure all submodules are updated!"
+
+mkdir -p processed/grouped
+
+for folder in "test" "train" "valid"
+do
+    grouped_file="processed/grouped/$folder"
+    
+    # group all yuml files
+    > "$grouped_file".yuml
+    for file in "processed/tokenized/$folder/"*.yuml
+    do
+        tr "\n" " " < "$file" >> "$grouped_file".yuml
+        echo "" >> "$grouped_file".yuml
+    done
+
+    # group all text files
+    > "$grouped_file".txt
+    for file in "data/hand/$folder/"*.txt
+    do
+        tr "\n" " " < "$file" >> "$grouped_file".txt
+        echo "" >> "$grouped_file".txt
+    done
+done
